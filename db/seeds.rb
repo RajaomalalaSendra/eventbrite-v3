f262734b5ab756e1b90f149e1eedcbe85feeb7c5
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-11 22:25:01 +0300

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-100.times do |i|
+2.times do |i|
 	User.create(email: "email#{i}@email.email", first_name: "User#{i}", last_name: "Last Name#{i}", description: "Description#{i} blablabla", encrypted_password: "encrypted_password_number#{i}")
 	Event.create(start_date: "12122019", duration: "#{30}", title: "Title#{i} title#{i} of event#{i}", description: "description#{i} of the event#{i}", price: "#{i}", location: "location#{i}")
 	Attendance.create(stripe_customer_id: "stripe user id#{i}")
