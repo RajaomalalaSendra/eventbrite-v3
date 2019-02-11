8b7bdff1a769ad3dd8e82ff710793879e82d3c69
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-11 22:54:20 +0300

    creation of db:seed

--- app/mailers/attendance_mailer.rb
+++ app/mailers/attendance_mailer.rb
@@ -1,2 +1,15 @@
 class AttendanceMailer < ApplicationMailer
+  default from: 'no-reply@zanfoana.com'
+ 
+  def welcome_email(attendant)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @attendant = attendant 
+    @user = user
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'http://zanfoana.com/login' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user.find(@attendant.user_id).email, subject: 'Bienvenue chez nous !') 
+  end
 end
