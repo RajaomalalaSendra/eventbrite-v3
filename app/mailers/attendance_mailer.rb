fb0a37893988661f1e1c8c798704e09e486ef80d
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-11 23:43:40 +0300

    this is the database

--- app/mailers/attendance_mailer.rb
+++ app/mailers/attendance_mailer.rb
@@ -4,11 +4,12 @@ class AttendanceMailer < ApplicationMailer
   def welcome_email(attend)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @attend = attend
+    @user = User.find(@attend.user_id)
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'http://zanfoana.com/login' 
+    @url  = 'http://zanfoana.com/event' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: Event.find(@attend.event_id).title, subject: 'Notre event pour aujourd hui !') 
+    mail(to: @user.email, subject: 'Notre event pour aujourd hui !') 
   end
 end
