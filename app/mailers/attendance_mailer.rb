13a75f88ca25757fc3d1e0420c1cebf3a134798b
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-11 22:37:57 +0300

    attendance mailer

---
+++ app/mailers/attendance_mailer.rb
@@ -0,0 +1,2 @@
+class AttendanceMailer < ApplicationMailer
+end
--- test/mailers/previews/attendence_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/attendence_mailer
-class AttendenceMailerPreview < ActionMailer::Preview
-
-end
