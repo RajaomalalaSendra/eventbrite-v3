f4ebd3119cc3331909acb798613f77a461226fa1
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-13 11:01:36 +0300

    model events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,23 +3,23 @@ class Event < ApplicationRecord
   has_many :attendances
   has_many :users, through: :attendances
   belongs_to :user
-  validates :start_date, presence: true, :create_event
-  validates :duration, presence: true, :create_event
+  validates :start_date, presence: true
+  validates :duration, presence: true
   validates :title, presence: true, length: {in: 5..140}
   validates :description, presence: true, length: {in: 20..1000}
-  validates :price, presence: true, length: {in: 1..1000}, :create_event
+  validates :price, presence: true, length: {in: 1..1000}
   validates :location, presence: true
   
   def create_event
-  	event = Event.new(params)
+  	event = Event.new()
   	if params[:start_date] < Time.now
-  	  puts "Erreur, pas possible de modifier ou créer cet évenement"
+  	  errors.add("Erreur, pas possible de modifier ou créer cet évenement")
   	elsif params[:duration] % 5 != 0 || params[:duration] < 0
-  	  puts "Erreur, pas possible de créer un événement non divisible à 5"
+  	  errors.add("Erreur, pas possible de créer un événement non divisible à 5")
   	elsif params[:price] == 0
-  	  puts "Erreur, pas d'événement gratuit pour le moment"
-  	elsif event.create?
-  	  puts "Utilisateur bien créé !"
+  	  errors.add("Erreur, pas d'événement gratuit pour le moment")
+  	else 
+      event.create?
   	end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 2.times do |i|
-	User.create(email: "email#{i}@email.email", first_name: "User#{i}", last_name: "Last Name#{i}", description: "Description#{i} blablabla", encrypted_password: "encrypted_password_number#{i}")
+	User.create(email: "email#{i}@email.email", first_name: "User#{i}", last_name: "Last Name#{i}", description: "Description#{i} blablabla", password: "blabla")
 	Event.create(start_date: "12/12/2019", duration: 30, title: "Title#{i} title#{i} of event#{i}", description: "description#{i} of the event#{i}", price: 10, location: "location#{i}", user_id: i.to_i + 1)
 	Attendance.create(stripe_customer_id: "stripe user id#{i}", user_id: i.to_i + 1, event_id: i.to_i + 1)
 end
