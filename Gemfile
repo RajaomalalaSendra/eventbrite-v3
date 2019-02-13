290f549a2fd82696594b2a931fbe98f31ab33d37
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-13 08:28:53 +0300

    the gemfile devise

--- Gemfile
+++ Gemfile
@@ -62,3 +62,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'letter_opener'
 gem 'dotenv-rails'
+gem 'devise'
--- db/migrate/20190211111642_create_users.rb
+++ db/migrate/20190211111642_create_users.rb
@@ -1,8 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :encrypted_password
       t.text :description
       t.string :first_name
       t.string :last_name
