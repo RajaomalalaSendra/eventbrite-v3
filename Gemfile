5d8b016e30f0af3d08129982abae397abd4aeeb9
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-13 16:16:37 +0300

    Gemfile and jquery

--- Gemfile
+++ Gemfile
@@ -63,3 +63,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'letter_opener'
 gem 'dotenv-rails'
 gem 'devise'
+gem 'jquery-rails'
+gem 'jquery'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'events#index'
+
+  devise_for :users
   resources 'events'
   resources 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
