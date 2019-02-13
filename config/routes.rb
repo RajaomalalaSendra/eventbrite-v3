ec9f39ea246b5ca7652df70cf7533d6a625e946c
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-13 09:05:53 +0300

    the devise destroyed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'events/index'
-  get 'events/show'
-  devise_for :users
+  root 'events#index'
+  resources 'events'
+  resources 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
