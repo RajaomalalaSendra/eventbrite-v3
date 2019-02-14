0128afac42fb6aac6301a2d2fdd5717e7456f39d
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-14 09:34:47 +0300

    edit the event_controller.rb and new.html.erb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,9 +5,8 @@ class EventsController < ApplicationController
   	@event = Event.new
   end
   def create
-  	puts "aaaaaaaaaaaaaaaaaaaaa"
-  	#@event = Event.new
-  	#@event.save()
+  	@event = Event.new(params)
+  	@event.save
   	redirect_to "/"
   end
   def show
