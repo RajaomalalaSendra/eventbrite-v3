608b5c27b3e2622de6f229995e36645275da1738
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-14 07:31:40 +0300

    some modification

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,15 @@
 class EventsController < ApplicationController
   def index
   end
-
+  def new
+  	@event = Event.new
+  end
+  def create
+  	@event = Event.new
+  	@event.save()
+  	redirect to "/"
+  end
   def show
   end
-  def new
-  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
+
+  def new
+  	@user = User.new
+  end
+  def create
+  	@user = User.new
+  	@user.create(firt_name: params[:first_name], last_name: params[:last_name], description: params[:description], email: params[:email], password: params[:password] )
+  	redirect_to root_path
+  end
   def show
   end
 
   def edit
+  end
+  def update
   end
 end
