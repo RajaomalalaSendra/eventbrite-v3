5cfb8568bc1e0edabbc111f71744b081804fc4f8
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-14 15:30:16 +0300

    the creation of the stripe data

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,10 @@
+require 'dotenv'
+
+Dotenv.load
+
+Rails.configuration.stripe = {
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'events#index'
 
   devise_for :users
-  resources 'events'
-  resources 'users'
+  resources :events
+  resources :users
+  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
